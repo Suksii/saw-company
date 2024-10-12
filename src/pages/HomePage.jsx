7e98100faff2975eb6255a4300d3b633205114a4
unknown <ramovic225@gmail.com>
2024-10-12 22:34:29 +0200

    animation added in header section

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import construction from "../assets/images/construction.jpg";
 import { MdArrowBackIos } from "react-icons/md";
 import { MdArrowForwardIos } from "react-icons/md";
+import { motion } from "framer-motion"
 
 const HomePage = () => {
 
@@ -31,9 +32,13 @@ const HomePage = () => {
     return (
         <div className="h-[70vh] flex items-center justify-center bg-cover bg-center relative" style={{ backgroundImage: `url(${construction})` }}>
             <div className="absolute inset-0 bg-black bg-opacity-50"></div>
-            <MdArrowBackIos className="absolute top 1/2 translate-y-1/2 left-[25%] text-blue-50 cursor-pointer" size={38} onClick={showPrev}/>
-            <MdArrowForwardIos className="absolute top 1/2 translate-y-1/2 right-[25%] text-blue-50 cursor-pointer" size={38} onClick={showNext}/>
-            <p className="relative text-white text-4xl font-bold">{titles[shownIndex]}</p>
+            <MdArrowBackIos className="absolute top 1/2 translate-y-1/2 left-[25%] text-blue-50 cursor-pointer hover:text-blue-300 duration-300" size={38} onClick={showPrev} />
+            <MdArrowForwardIos className="absolute top 1/2 translate-y-1/2 right-[25%] text-blue-50 cursor-pointer hover:text-blue-300 duration-300" size={38} onClick={showNext} />
+            <motion.p key={shownIndex}
+                initial={{ opacity: 0, scale: 0 }}
+                animate={{ opacity: 1, scale: 1 }}
+                exit={{ opacity: 0, scale: 0 }}
+                transition={{ duration: 1.2 }} className="relative text-white text-4xl font-bold">{titles[shownIndex]}</motion.p>
         </div>
     );
 };
