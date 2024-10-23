9ba4bd97242657bef4e09f4594a8356178349226
unknown <ramovic225@gmail.com>
2024-10-23 20:17:34 +0200

    Header reusable component added in Contact Page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,14 @@
+import HeaderTitle from "../components/HeaderTitle";
+
 const Contact = () => {
-    return (
-        <div>
-            <p>Contact</p>
-        </div>
-    )
-}
+  return (
+    <div>
+      <HeaderTitle
+        title="Kontakt"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate"
+      />
+    </div>
+  );
+};
 
 export default Contact;
