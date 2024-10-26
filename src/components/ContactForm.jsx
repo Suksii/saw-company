00da444ba634ad6fdde08cf01a8a0afbbf6d4c2e
unknown <ramovic225@gmail.com>
2024-10-26 17:43:41 +0200

    label positioning when  non-empty field fixed

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -2,8 +2,21 @@ import { AiFillMail } from "react-icons/ai";
 import { FaMapMarkerAlt, FaPhoneAlt } from "react-icons/fa";
 import Input from "./Input";
 import Textarea from "./Textarea";
+import { useState } from "react";
 
 const ContactForm = () => {
+
+    const [fullName, setFullName] = useState("");
+    const [email, setEmail] = useState("");
+    const [subject, setSubject] = useState("");
+    const [message, setMessage] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(fullName, email, subject, message);
+        
+    }
+
   return (
     <div className="flex flex-col md:flex-row bg-gradient-to-r from-blue-200 via-blue-100 to-blue-200 gap-6 items-center py-8">
       <div className="w-full md:w-1/2">
@@ -43,19 +56,19 @@ const ContactForm = () => {
             kontaktirate. Naš tim stručnjaka je tu da vam pomogne.
           </p>
         </div>
-        <form className="flex flex-col gap-6 w-[80%] mx-auto pt-4">
+        <form className="flex flex-col gap-6 w-[80%] mx-auto pt-4" onSubmit={handleSubmit}>
           <div className="flex flex-col md:flex-row gap-6">
-            <Input type="text" label="Your Full Name" />
-            <Input type="email" label="Your Email" />
+            <Input type="text" label="Your Full Name" value={fullName} onChange={(e) => setFullName(e.target.value)}/>
+            <Input type="email" label="Your Email" value={email} onChange={(e) => setEmail(e.target.value)}/>
           </div>
           <div className="w-full">
-            <Input type="text" label="Subject" />
+            <Input type="text" label="Subject" value={subject} onChange={(e) => setSubject(e.target.value)}/>
           </div>
           <div className="w-full">
-            <Textarea label="Message"/>
+            <Textarea label="Message" value={message} onChange={(e) => setMessage(e.target.value)}/>
           </div>
-          <button className="w-48 bg-blue-900 py-2 rounded-sm text-blue-50 tracking-wider font-semibold">
-            Submit
+          <button type="submit" className="w-48 bg-blue-900 py-2 rounded-sm text-blue-50 tracking-wider font-semibold">
+            Pošaljite
           </button>
         </form>
       </div>
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,13 +1,21 @@
-const Input = ({ label, type, onChange }) => {
+const Input = ({ label, type, onChange, value }) => {
   return (
     <div className="relative w-full">
       <input
         type={type}
+        value={value}
         placeholder=" "
-        className="w-full p-2 bg-transparent focus:outline-none border-b border-b-blue-900 outline-none peer"
+        className="w-full p-2 bg-transparent focus:outline-none border-b border-b-blue-900 outline-none peer text-blue-900"
         onChange={onChange}
       />
-      <label className="absolute left-2 top-1/2 transform -translate-y-1/2 text-blue-900 transition-all duration-200 pointer-events-none peer-placeholder-shown:top-1/2 peer-placeholder-shown:left-2 peer-focus:-top-2 peer-focus:left-1 peer-focus:text-sm peer-focus:text-blue-600">
+      <label
+        className={`absolute left-2 top-1/2 transform -translate-y-1/2 text-blue-900 transition-all duration-200 pointer-events-none
+        ${
+          value
+            ? "-top-2 left-1 text-sm text-blue-600"
+            : "peer-placeholder-shown:top-1/2 peer-placeholder-shown:left-2 peer-focus:-top-2 peer-focus:left-1 peer-focus:text-sm peer-focus:text-blue-600"
+        }`}
+      >
         {label}
       </label>
     </div>
