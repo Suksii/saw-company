b380a402a8faee478c89139f4e7a582dd2c3efb2
unknown <ramovic225@gmail.com>
2024-10-26 14:19:25 +0200

    form styled better

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -3,7 +3,7 @@ import { FaMapMarkerAlt, FaPhoneAlt } from "react-icons/fa";
 
 const ContactForm = () => {
   return (
-    <div className="flex flex-col md:flex-row gap-6 items-center py-8">
+    <div className="flex flex-col md:flex-row bg-gradient-to-r from-blue-200 via-blue-100 to-blue-200 gap-6 items-center py-8">
       <div className="w-full md:w-1/2">
         <div className="flex flex-col gap-6 justify-center text-center w-2/3 mx-auto">
           <h2 className="text-2xl">Kontakt informacije</h2>
@@ -31,21 +31,21 @@ const ContactForm = () => {
       <div className="w-full md:w-1/2">
         <div className="flex flex-col justify-center items-center gap-4 text-center w-2/3 mx-auto">
           <h3 className="text-2xl">Kontaktirajte nas putem forme</h3>
-          <p className="text-lg">
+          <p className="text-lg pb-2">
             Imate pitanja, želite saznati više o našim uslugama ili vam je
             potrebna dodatna informacija? Ne ustručavajte se da nas
             kontaktirate. Naš tim stručnjaka je tu da vam pomogne.
           </p>
         </div>
         <form className="flex flex-col gap-6 w-[80%] mx-auto pt-4">
-          <div className="flex flex-col md:flex-row gap-4">
+          <div className="flex flex-col md:flex-row gap-6">
             <div className="relative w-full">
               <input
                 type="text"
                 placeholder=" "
-                className="w-full p-2 bg-blue-50 focus:outline-none outline-none border border-gray-300 rounded-md focus:border-blue-400 peer"
+                className="w-full p-2 bg-transparent focus:outline-none border-b border-b-blue-900 outline-none peer"
               />
-              <label className="absolute left-2 top-1/2 transform -translate-y-1/2 text-gray-400 transition-all duration-200 pointer-events-none peer-placeholder-shown:top-1/2 peer-placeholder-shown:left-2 peer-focus:-top-2 peer-focus:left-1 peer-focus:text-sm peer-focus:text-blue-500">
+              <label className="absolute left-2 top-1/2 transform -translate-y-1/2 text-blue-900 transition-all duration-200 pointer-events-none peer-placeholder-shown:top-1/2 peer-placeholder-shown:left-2 peer-focus:-top-2 peer-focus:left-1 peer-focus:text-sm peer-focus:text-blue-500">
                 Your Full Name
               </label>
             </div>
@@ -53,9 +53,9 @@ const ContactForm = () => {
               <input
                 type="text"
                 placeholder=" "
-                className="w-full p-2 bg-blue-50 focus:outline-none outline-none border border-gray-300 rounded-md focus:border-blue-400 peer"
+                className="w-full p-2 bg-transparent focus:outline-none border-b border-b-blue-900 outline-none peer"
               />
-              <label className="absolute left-2 top-1/2 transform -translate-y-1/2 text-gray-400 transition-all duration-200 pointer-events-none peer-placeholder-shown:top-1/2 peer-placeholder-shown:left-2 peer-focus:-top-2 peer-focus:left-1 peer-focus:text-sm peer-focus:text-blue-500">
+              <label className="absolute left-2 top-1/2 transform -translate-y-1/2 text-blue-900 transition-all duration-200 pointer-events-none peer-placeholder-shown:top-1/2 peer-placeholder-shown:left-2 peer-focus:-top-2 peer-focus:left-1 peer-focus:text-sm peer-focus:text-blue-600">
                 Your Email
               </label>
             </div>
@@ -65,9 +65,9 @@ const ContactForm = () => {
               <input
                 type="text"
                 placeholder=" "
-                className="w-full p-2 bg-blue-50 focus:outline-none outline-none border border-gray-300 rounded-md focus:border-blue-400 peer"
+                className="w-full p-2 bg-transparent focus:outline-none border-b border-b-blue-900 outline-none peer"
               />
-              <label className="absolute left-2 top-1/2 transform -translate-y-1/2 text-gray-400 transition-all duration-200 pointer-events-none peer-placeholder-shown:top-1/2 peer-placeholder-shown:left-2 peer-focus:-top-2 peer-focus:left-1 peer-focus:text-sm peer-focus:text-blue-500">
+              <label className="absolute left-2 top-1/2 transform -translate-y-1/2 text-blue-900 transition-all duration-200 pointer-events-none peer-placeholder-shown:top-1/2 peer-placeholder-shown:left-2 peer-focus:-top-2 peer-focus:left-1 peer-focus:text-sm peer-focus:text-blue-600">
                 Subject
               </label>
             </div>
@@ -77,9 +77,9 @@ const ContactForm = () => {
               <textarea
                 type="text"
                 placeholder=" "
-                className="w-full p-2 bg-blue-50 focus:outline-none outline-none border border-gray-300 rounded-md focus:border-blue-400 peer"
+                className="w-full p-2 bg-transparent focus:outline-none border-b border-b-blue-900 outline-none peer"
               />
-              <label className="absolute left-2 top-2 transform -translate-y-1/2 text-gray-400 transition-all duration-200 pointer-events-none peer-placeholder-shown:top-4 peer-placeholder-shown:left-2 peer-focus:-top-2 peer-focus:left-1 peer-focus:text-sm peer-focus:text-blue-500">
+              <label className="absolute left-2 top-2 transform -translate-y-1/2 text-blue-900 transition-all duration-200 pointer-events-none peer-placeholder-shown:top-4 peer-placeholder-shown:left-2 peer-focus:-top-2 peer-focus:left-1 peer-focus:text-sm peer-focus:text-blue-600">
                 Message
               </label>
             </div>
